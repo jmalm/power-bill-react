1f04415e94285c89b45f9cde2a6190b87482d537
Jakob Malm <jakob.malm@gmail.com>
2025-08-04 23:36:24 +0200

    Increase page width

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="w-full max-w-md">
+      <div className="w-full max-w-lg">
         <h1 className="text-3xl font-bold mb-6 text-center">
           Calculate Electricity Cost
         </h1>
